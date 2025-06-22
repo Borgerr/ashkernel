2c462a5d591ffccce567963c818833cff38c1bb7
ash <ashkernel02@gmail.com>
2025-06-21 18:30:28 -0600

    basic exception handling

--- sys/kernel.c
+++ sys/kernel.c
@@ -4,23 +4,11 @@
 
 extern uint8_t __bss[], __bss_end[];	// taken from kernel.ld
 
-/*
-void sbi_console_putstr(char *s)
-{
-     * Puts a null-terminated string.
-     * NOT a part of the OpenSBI spec afaict, but a nice encoding.
-    char *current = s;
-    while (*current)
-    {
-        putchar(*current);
-        current++;
-    }
-}
-*/
-
 void kernel_main(void)
 {
 	memset(__bss, 0, (size_t) __bss_end - (size_t) __bss);  // set bss to 0 as a sanity check
+    WRITE_CSR(stvec, (uint32_t) kernel_entry);
+    __asm__ __volatile__("unimp");
 
     char *hello = "\r\nHello, World!\r\n";
     printf(hello);
