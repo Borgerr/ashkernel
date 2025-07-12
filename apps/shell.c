4269e6a1c6fb5918bdc2ffdbca684d34e905ebbf
ash <ashkernel02@gmail.com>
2025-07-12 17:32:35 -0600

    "shell" that tests previously implemented syscalls

--- apps/shell.c
+++ apps/shell.c
@@ -1,9 +1,35 @@
 #include "../common.h"
 
+// kind of works more like a terminal emulator
+// but really currently just for verifying reading and writing characters
 void main(void)
 {
-    // TODO: print stack pointer throughout program
-    //putchar('a');
-    printf("oh my fucking god it's a user process\n");
-    for (;;) ;
+    while (1) {
+        printf("-> ");
+        char cmdline[128];      // XXX: input stored on the stack
+                                // probably safe since we have a check for the length
+        for (int i = 0;; i++) {
+            char ch = getchar();
+            putchar(ch);
+            if (i == sizeof(cmdline) - 1) {
+                printf("\n\nhold up youngster, you're yapping too much\n\n");
+                break;
+            } else if (ch == '\r') {
+                printf("\n");
+                cmdline[i] = '\0';
+                break;
+            } else {
+                cmdline[i] = ch;
+            }
+        }
+
+        if (strcmp(cmdline, "hello") == 0)
+            printf("hey!\n");
+        else if (strcmp(cmdline, "break shit") == 0)    // XXX: TESTING ONLY. Remove once you want shit to not break.
+            break;
+        else
+            printf("unrecognized command: %s\n", cmdline);
+    }
+    // user/user.c start (previous function in the backtrace)
+    // calls the exit syscall after this one
 }
